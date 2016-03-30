3f8debc4dac00290c4ad6ff20d3e59f97a270b16
ric murillo <ric@lootsie.com>
2016-03-30 15:50:47 -0700

    dockerfile 01

--- api/main.py
+++ api/main.py
@@ -7,9 +7,12 @@ import os
 class MainHandler:
 	"""docstring for MainHandler"""
 	def on_get(self, req, resp):
-		data = "Hello Welcome to Docker -> Nginx -> Gunicorn -> Falcon"
-		resp.body = data
-
+		try:
+			data = "Hello Welcome to Docker -> Nginx -> Gunicorn -> Falcon"
+			resp.body = data
+		except Exception, e:
+			print e
+			resp.body = "error"
  
 api = falcon.API()
 api.add_route('/', MainHandler())
